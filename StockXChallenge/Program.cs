c19c66342bee9b509a8e21944fac4b130334218f
Allen <allenarcenal@gmail.com>
2016-03-08 08:45:24 +0800

    latest

--- StockXChallenge/Program.cs
+++ StockXChallenge/Program.cs
@@ -19,9 +19,81 @@ namespace StockXChallenge
 
             string connectionString = "Data Source=10.11.18.21;User ID=sa;"
                                                + "Password=password-2014;";
-            string qString =
 
-                "";
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("SET NOCOUNT ON   ");
+            sb.Append(" ;   ");
+            sb.Append(" IF OBJECT_ID('tempdb.dbo.#TempOrds2') IS NOT NULL   ");
+            sb.Append(" DROP TABLE tempdb.dbo.#TempOrds2    ");
+            sb.Append(" IF OBJECT_ID('tempdb.dbo.#TempMatchedOrders') IS NOT NULL   ");
+            sb.Append(" DROP TABLE  tempdb.dbo.#TempMatchedOrders   ");
+            sb.Append(" ;   ");
+            sb.Append(" SELECT DISTINCT");
+            sb.Append("     OTWC.username,o.ParentOrdID,o.OrdStatus AS \"OrderStatus\",o.TransactTime AS \"OrderDate\",");
+            sb.Append("     IsNull(a.AccountCode,'') AS \"WebID\",isNull([OT-WebTrading].[dbo].FXfunc_GetCFESide(o.Side),'') AS \"Side\",");
+            sb.Append("     isNull([OT-WebTrading].[dbo].FXfunc_GetCFEBoard(SUBSTRING(o.Symbol,1,1)),'') AS \"Board\",");
+            sb.Append("     isNull([OT-WebTrading].[dbo].FXfunc_GetCFETerms(o.TimeInForce),'') AS Terms,IsNull(SUBSTRING(o.Symbol,2,10),'') AS \"Security\",");
+            sb.Append("     IsNull(o.Price,0) AS \"Price\",	IsNull(o.OrderQty,0) AS \"Vol\",IsNull(o.LeavesQty,0) AS \"PubVol\",");
+            sb.Append("     IsNull(o.CumQty,0) AS \"MatchedVol\",IsNull(CONVERT(varchar(8),o.ExpireDate,112),'') AS \"Expiration\",");
+            sb.Append("     IsNull(a.AccountCode,'') AS \"AccountID\",ISNULL(o.UnfilledAmount,0) AS \"UnfilledAmount\"");
+            sb.Append(" INTO #TempOrds2");
+            sb.Append(" FROM [TEDS-FIX].[TEDS-FIX].dbo.fxOrderSnapshot  o INNER JOIN [TEDS-FIX].[TEDS-FIX].[dbo].fxOrderPartySnapshot p	on (o.parentordid = p.parentordid) ");
+            sb.Append(" INNER JOIN [TEDS-FIX].[TEDS-FIX].[dbo].Accounts a on (a.XchgAccountCode = p.ID) INNER JOIN [OT-WebComp].[dbo].tbl_ot_login OTWC on (OTWC.XchgAccountCode = p.ID)");
+            sb.Append(" WHERE ordstatus IN ('0','5') AND CONVERT(varchar(8),ExpireDate,112) >= CONVERT(VARCHAR(8),GETDATE(),112)");
+            sb.Append(" AND p.[Role] = 24 AND a.OnlineTrading = 1 ");
+            sb.Append(" ;   ");
+            sb.Append(" SELECT o.username AS \"USERID\", o.WebID AS \"ACCOUNTCODE\", o.ParentOrdID AS \"PARENTORDERID\",");
+            sb.Append("     o.OrderDate AS \"ORDERDATE\", o.Side AS \"SIDE\",'MAIN' as BOARDLOT, o.[Security] AS \"STOCKCODE\",");
+            sb.Append("     o.[Price] AS \"PRICE\", o.Vol AS \"QUANTITY\", o.Terms AS \"TERM\"");
+            sb.Append(" INTO #TempMatchedOrders");
+            sb.Append(" FROM #TempOrds2 o");
+            sb.Append(" INNER JOIN [OT-WebQuotes].dbo.QUOTESPH_SECURITIES_REV q on");
+            sb.Append(" ((o.[Security] = q.Seccode and o.Price >= q.LastPrice and o.[side] = 'B') or");
+            sb.Append(" (o.[Security] = q.Seccode and o.Price <= q.LastPrice and o.[side] = 'S'))");
+            sb.Append(" WHERE o.Board = 3");
+            sb.Append(" AND q.LastPrice <> 0");
+            sb.Append(" UNION ALL");
+            sb.Append(" SELECT	o.username AS \"USERID\", o.WebID AS \"ACCOUNTCODE\", o.ParentOrdID AS \"PARENTORDERID\", o.OrderDate AS \"ORDERDATE\",");
+            sb.Append(" o.Side AS \"SIDE\", 'ODDLOT' as BOARDLOT, o.[Security] AS \"STOCKCODE\", o.[Price] AS \"PRICE\",");
+            sb.Append(" o.Vol AS \"QUANTITY\"	, o.Terms AS \"TERM\"");
+            sb.Append(" FROM #TempOrds2 o");
+            sb.Append(" INNER JOIN [OT-WebQuotes].dbo.QUOTESPH_SECURITIES_REV_ODDLOT q on");
+            sb.Append(" ((o.[Security] = q.Seccode and o.Price >= q.LastPrice and o.[side] = 'B') or");
+            sb.Append(" (o.[Security] = q.Seccode and o.Price <= q.LastPrice and o.[side] = 'S'))");
+            sb.Append(" WHERE o.Board = 4");
+            sb.Append(" AND q.LastPrice <> 0");
+            sb.Append(" ;  ");
+            sb.Append(" SELECT [USERID] AS \"USER ID\",[ACCOUNTCODE] AS \"ACCOUNT CODE\",[STOCKCODE] AS \"STOCKCODE\",[BOARDLOT] AS BOARDLOT,");
+            sb.Append(" CASE WHEN SUM(CASE WHEN [SIDE] = 'B' THEN 1 ELSE 0 END) > 1 THEN ");
+            sb.Append("     SUM(CASE WHEN [SIDE] = 'B' THEN CONVERT(DECIMAL(19,9),QUANTITY) * CONVERT(DECIMAL(19,9),PRICE) ELSE 0 END) /");
+            sb.Append("     SUM(CASE WHEN [SIDE] = 'B' THEN CONVERT(DECIMAL(19,9),QUANTITY) ELSE 0 END)");
+            sb.Append(" ELSE SUM(CASE WHEN [SIDE] = 'B' THEN CONVERT(DECIMAL(19,9),PRICE) ELSE 0 END)");
+            sb.Append(" END");
+            sb.Append(" AS \"AVERAGE PRICE(BUY)\",");
+            sb.Append(" CASE WHEN SUM(CASE WHEN [SIDE] = 'S' THEN 1 ELSE 0 END) > 1 THEN  ");
+            sb.Append(" SUM(CASE WHEN [SIDE] = 'S' THEN CONVERT(DECIMAL(19,9),QUANTITY) * CONVERT(DECIMAL(19,9),PRICE) ELSE 0 END) /");
+            sb.Append(" SUM(CASE WHEN [SIDE] = 'S' THEN CONVERT(DECIMAL(19,9),QUANTITY) ELSE 0 END)");
+            sb.Append(" ELSE");
+            sb.Append(" SUM(CASE WHEN [SIDE] = 'S' THEN CONVERT(DECIMAL(19,9),PRICE) ELSE 0 END)");
+            sb.Append(" END");
+            sb.Append(" AS \"AVERAGE PRICE(SELL)\",");
+            sb.Append(" SUM(CASE WHEN [SIDE] = 'B' THEN CONVERT(DECIMAL(19,9),QUANTITY) ELSE 0 END) -");
+            sb.Append(" SUM(CASE WHEN [SIDE] = 'S' THEN CONVERT(DECIMAL(19,9),QUANTITY) ELSE 0 END)	");
+            sb.Append(" AS \"NET QUANTITY\"");
+            sb.Append(" FROM #TempMatchedOrders");
+            sb.Append(" GROUP BY [USERID],[ACCOUNTCODE],[STOCKCODE],[BOARDLOT]");
+            sb.Append(" ORDER BY [USERID],[ACCOUNTCODE],[STOCKCODE],[BOARDLOT]");
+            sb.Append(" ;  ");
+            sb.Append(" UPDATE 	[TEDS-FIX].[TEDS-FIX].dbo.fxOrderSnapshot");
+            sb.Append(" SET	ExpireDate = GETDATE()");
+            sb.Append(" WHERE TimeInForce IN ('G','6') ");
+            sb.Append(" AND ParentOrdID");
+            sb.Append(" IN (SELECT PARENTORDERID FROM #TempMatchedOrders)");
+            sb.Append(" --SELECT CONVERT(VARCHAR,@@ROWCOUNT) AS \"Number of GTD Orders Modified\"");
+            
+
+            string qString = sb.ToString();
 
             using (SqlConnection connection =
             new SqlConnection(connectionString))
@@ -41,7 +113,10 @@ namespace StockXChallenge
                     SqlDataReader reader = command.ExecuteReader();
                     while (reader.Read())
                     {
-                        Console.WriteLine();
+                        foreach (var r in reader)
+                        {
+                            Console.WriteLine(r);
+                        }
                     }
                     reader.Close();
                 }
@@ -55,119 +130,7 @@ namespace StockXChallenge
             //sdb.ProcessOrders(orders);
             Console.ReadKey();
         }
-//        string ab = 
-//            "SET NOCOUNT ON"
-
-//            +"GO"
-	
-//    +"IF OBJECT_ID('tempdb.dbo.#TempOrds2') IS NOT NULL"
-//    +"	DROP TABLE tempdb.dbo.#TempOrds2"
-	
-//    +"IF OBJECT_ID('tempdb.dbo.#TempMatchedOrders') IS NOT NULL"
-//    +"	DROP TABLE  tempdb.dbo.#TempMatchedOrders"
-
-//    +"GO"
-//    +"	SELECT 	DISTINCT"
-//    +"		OTWC.username,o.ParentOrdID,o.OrdStatus AS \"OrderStatus\",o.TransactTime AS \"OrderDate\", "
-//    +"		IsNull(a.AccountCode,'') AS \"WebID\",isNull([OT-WebTrading].[dbo].FXfunc_GetCFESide(o.Side),'') AS \"Side\","
-//    +"		isNull([OT-WebTrading].[dbo].FXfunc_GetCFEBoard(SUBSTRING(o.Symbol,1,1)),'') AS \"Board\","
-//    +"		isNull([OT-WebTrading].[dbo].FXfunc_GetCFETerms(o.TimeInForce),'') AS Terms,IsNull(SUBSTRING(o.Symbol,2,10),'') AS \"Security\","
-//    +"		IsNull(o.Price,0) AS \"Price\",	IsNull(o.OrderQty,0) AS \"Vol\",IsNull(o.LeavesQty,0) AS \"PubVol\","
-//    +"		IsNull(o.CumQty,0) AS \"MatchedVol\",IsNull(CONVERT(varchar(8),o.ExpireDate,112),'') AS \"Expiration\","
-//    +"		IsNull(a.AccountCode,'') AS \"AccountID\",ISNULL(o.UnfilledAmount,0) AS \"UnfilledAmount\""
-			
-//    +"INTO #TempOrds2"
-			
-//    FROM [TEDS-FIX].[TEDS-FIX].dbo.fxOrderSnapshot  o 
-//    INNER JOIN [TEDS-FIX].[TEDS-FIX].[dbo].fxOrderPartySnapshot p	on (o.parentordid = p.parentordid) 
-//    INNER JOIN [TEDS-FIX].[TEDS-FIX].[dbo].Accounts a on (a.XchgAccountCode = p.ID)    
-//    INNER JOIN [OT-WebComp].[dbo].tbl_ot_login OTWC on (OTWC.XchgAccountCode = p.ID)    
-
-//    WHERE ordstatus IN ('0','5')
-//    AND CONVERT(varchar(8),ExpireDate,112) >= CONVERT(VARCHAR(8),GETDATE(),112)
-//    AND p.[Role] = 24
-//    AND a.OnlineTrading = 1 
-///**END OF RETRIEVAL**/	
-//GO
-///**REPORT 1: TRADE LIST**/				
-//    SELECT	o.username AS "USERID",
-//            o.WebID AS "ACCOUNTCODE",
-//            o.ParentOrdID AS "PARENTORDERID",
-//            o.OrderDate AS "ORDERDATE",
-//            o.Side AS "SIDE",
-//            'MAIN' as BOARDLOT,
-//            o.[Security] AS "STOCKCODE",	
-//            o.[Price] AS "PRICE",	
-//            o.Vol AS "QUANTITY",
-//            o.Terms AS "TERM"
-//    INTO 	#TempMatchedOrders
-//    FROM 	#TempOrds2 o
-//    INNER JOIN [OT-WebQuotes].dbo.QUOTESPH_SECURITIES_REV q on 
-//    (
-//        (o.[Security] = q.Seccode and o.Price >= q.LastPrice and o.[side] = 'B') 
-//            or 
-//        (o.[Security] = q.Seccode and o.Price <= q.LastPrice and o.[side] = 'S')
-//    )
-//    WHERE o.Board = 3
-//    AND q.LastPrice <> 0
-		
-//    UNION ALL
-		
-//    SELECT	o.username AS "USERID",
-//            o.WebID AS "ACCOUNTCODE",
-//            o.ParentOrdID AS "PARENTORDERID",
-//            o.OrderDate AS "ORDERDATE",
-//            o.Side AS "SIDE",
-//            'ODDLOT' as BOARDLOT,
-//            o.[Security] AS "STOCKCODE",	
-//            o.[Price] AS "PRICE",	
-//            o.Vol AS "QUANTITY"	,
-//            o.Terms AS "TERM"
-//    FROM 	#TempOrds2 o
-//    INNER JOIN [OT-WebQuotes].dbo.QUOTESPH_SECURITIES_REV_ODDLOT q on 
-//    (
-//        (o.[Security] = q.Seccode and o.Price >= q.LastPrice and o.[side] = 'B') 
-//            or 
-//        (o.[Security] = q.Seccode and o.Price <= q.LastPrice and o.[side] = 'S')
-//    )
-//    WHERE o.Board = 4
-//    AND q.LastPrice <> 0
-		
-//GO		
-//    SELECT	[USERID] AS "USER ID",
-//            [ACCOUNTCODE] AS "ACCOUNT CODE",
-//            [STOCKCODE] AS "STOCKCODE",	
-//            [BOARDLOT] AS BOARDLOT,		
-//                CASE WHEN SUM(CASE WHEN [SIDE] = 'B' THEN 1 ELSE 0 END) > 1 THEN  
-//                    SUM(CASE WHEN [SIDE] = 'B' THEN CONVERT(DECIMAL(19,9),QUANTITY) * CONVERT(DECIMAL(19,9),PRICE) ELSE 0 END) /
-//                    SUM(CASE WHEN [SIDE] = 'B' THEN CONVERT(DECIMAL(19,9),QUANTITY) ELSE 0 END)
-//                ELSE
-//                    SUM(CASE WHEN [SIDE] = 'B' THEN CONVERT(DECIMAL(19,9),PRICE) ELSE 0 END)
-//                END				
-//            AS "AVERAGE PRICE(BUY)",						
-//                CASE WHEN SUM(CASE WHEN [SIDE] = 'S' THEN 1 ELSE 0 END) > 1 THEN  
-//                    SUM(CASE WHEN [SIDE] = 'S' THEN CONVERT(DECIMAL(19,9),QUANTITY) * CONVERT(DECIMAL(19,9),PRICE) ELSE 0 END) /
-//                    SUM(CASE WHEN [SIDE] = 'S' THEN CONVERT(DECIMAL(19,9),QUANTITY) ELSE 0 END)
-//                ELSE
-//                    SUM(CASE WHEN [SIDE] = 'S' THEN CONVERT(DECIMAL(19,9),PRICE) ELSE 0 END)
-//                END				
-//            AS "AVERAGE PRICE(SELL)",	
-//                SUM(CASE WHEN [SIDE] = 'B' THEN CONVERT(DECIMAL(19,9),QUANTITY) ELSE 0 END) -
-//                SUM(CASE WHEN [SIDE] = 'S' THEN CONVERT(DECIMAL(19,9),QUANTITY) ELSE 0 END)				
-//            AS "NET QUANTITY"
-//    FROM #TempMatchedOrders
-//    GROUP BY [USERID],[ACCOUNTCODE],[STOCKCODE],[BOARDLOT]
-//    ORDER BY [USERID],[ACCOUNTCODE],[STOCKCODE],[BOARDLOT]
-
-//GO
-//    UPDATE 	[TEDS-FIX].[TEDS-FIX].dbo.fxOrderSnapshot
-//    SET		ExpireDate = GETDATE()
-//    WHERE	TimeInForce IN ('G','6') 
-//        AND ParentOrdID 
-//            IN (SELECT PARENTORDERID FROM #TempMatchedOrders)
-			
-//    --SELECT CONVERT(VARCHAR,@@ROWCOUNT) AS "Number of GTD Orders Modified"
-
+        
     }
 
 }
--- StockXChallenge/README.md
+++ StockXChallenge/README.md
@@ -1,5 +1,10 @@
 # StockXChallenge plan
 
+todo:
+1. create poscos table (portfolio balance)
+2. add sample files to the solution. client.txt, postcos, ledger 
+
+
 Filled Orders Report
 USERID|ACCOUNTCODE|ORDERTIME|SIDE|STOCKCODE|PRICE|QUANTITY
 
@@ -14,14 +19,47 @@ portfolio.com
 
 to maintain cash balance:
 pre req:
-1. create a table cash (auto table identity id must be present)
+1. create tables: (auto table identity id must be present) done
+	a. ClientAverageCost
+	b. MatchedOrder
+	c. Summay
 2. update entity model from db 
-1. initial load of cash -- DONE
+1. initial load of cash 
 	a. read cash.txt
 	b. get one line
 	c. parse with vertical bar
 	d. insert if not exist, or update if it is
-2. We process the filled orders text file.
+
+2. We process the filled orders query to 10.11.15.21 server.
+	a. run query via StockXDBController
+	b. get one line from reader
+	c. save each line using date, volume, stock, account code composite key
+	d. update 
+
+
+3. Process each filled orders by creating a ledger:
+	a. run query via StockXDBController
+	b. get one line from reader
+	c. append each line to a csv.
+	d. format is this: example of format.
+
+4. We process the summary orders query to 10.11.15.21 server.
+	a. run query via StockXDBController
+	b. get one line from reader
+	c. save each line using date, UserId, AccountCode, StockCode, BoardLot, AvePriceBuy, AvePriceSell, NetQuantity.
+	d. Use date, UserId, AccountCode, StockCode as composite key 
+	e. 
+
+5. Process each summary orders by creating a postcost:
+	a. run query via StockXDBController
+	b. get one line from reader
+	c. update balance in portfolio ( we might need a portfolio table. use same columns as poscost) 
+	d. create a new csv
+	c. append each line to a csv.
+	d. format is this: example of format
+
+
+2. (deprecated) We process the filled orders text file.
 	a. read filledorders.txt
 	b. get one line from array
 	c. parse with vertical bar or comma
